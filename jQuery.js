0c44bf378eb8ff5464373b8e5339da5b9c6d8960
John <johnopana2016@gmail.com>
2019-11-08 14:23:00 +0300

    add row

---
+++ jQuery.js
@@ -0,0 +1,44 @@
+ $(document).ready(function() {
+    $(".clickable").click(function() {
+      $("#design-showing").slideToggle();
+      $("#design-hidden").slideToggle();
+    });
+   });
+   
+   $(document).ready(function() {
+    $(".click").click(function() {
+      $("#dev-showing").slideToggle();
+      $("#dev-hidden").slideToggle();
+    });
+   });
+   
+   $(document).ready(function() {
+    $(".clik").click(function() {
+      $("#pro-showing").slideToggle();
+      $("#pro-hidden").slideToggle();
+    });
+   });
+   
+   
+    $(document).ready(function () {
+      return $('#contact-form').submit(function (e) {
+        var email, message, name;
+        name = document.getElementById('inputName');
+        email = document.getElementById('inputEmail');
+        message = document.getElementById('inputMessage');
+        if (!name.value || !email.value || !message.value) {
+          alert('Please check your entries');
+          return false;
+        } else {
+          $.ajax({
+            method: 'POST',
+            url: "http://formspree.io/rick.ndonga@gmail.com",
+            data: $('#contact-form').serialize(),
+            datatype: 'json' });
+   
+          e.preventDefault();
+          $(this).get(0).reset();
+          return alert('Message sent');
+        }
+      });
+    });
