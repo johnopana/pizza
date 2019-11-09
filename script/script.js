7f5f1f38886604b314adc47fe31fe5e9abdb8bc5
John <johnopana2016@gmail.com>
2019-11-09 13:46:18 +0300

    modified : script/script.js

--- script/script.js
+++ script/script.js
@@ -8,11 +8,11 @@ const toppingDivs = document.querySelectorAll('.topping')
 const crustDivs = document.querySelectorAll('.crust')
 const toppingPrices = {
     capsicum :15,
-    periperichicken:26,
+    periPeriChicken:26,
     barberque :21,
     sausage:18,
-    chickentikka:25,
-    grilledrasher:20,
+    chiCkenTikka:25,
+    grilledRasher:20,
 }
 const crustPrices = {
     classicHandTossed:34,
@@ -21,3 +21,13 @@ const crustPrices = {
     freshPan:40,
     newHandTossed:40,
 }
+let totalCost, toppingCost,cheeseCost,getCost,grandCost;
+const RN = num => { return Math.round(num * 100) / 100 }
+const toggleClass = (item, c) => {
+    item.classList.contains(c) ? item.classList.remove(c) : item.classList.add(c)
+}
+const camelize = str => {
+    return str.toLowerCase().replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
+      return index == 0 ? letter.toLowerCase() : letter.toUpperCase()
+    }).replace(/\s+/g, '')
+  }
