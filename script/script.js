cb16bc662781e6ccee1834573372cccbe2b4e8b7
John <johnopana2016@gmail.com>
2019-11-11 12:00:36 +0300

    delete variable

--- script/script.js
+++ script/script.js
@@ -8,3 +8,13 @@ var chickenRasher=document.getElementById("img6") .value;
 var total= ("img1"+"img2"+"img3"+"img4"+"img5"+"img6");
 document.getElementById("total").value="total";
 }
+let selectedToppings = []
+const totalCostBox = document.getElementById('total-cost')
+const toppingsCostBox = document.getElementById('toppings-cost')
+const cheeseCostBox = document.getElementById('cheese-cost')
+const gstCostBox = document.getElementById('gst-cost')
+const grandCostBox = document.getElementById('grand-cost')
+const quantity = document.getElementById('quantity')
+const toppingDivs = document.querySelectorAll('.topping')
+const crustDivs = document.querySelectorAll('.crust')
+
