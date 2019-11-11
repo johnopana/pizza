ea4990bb2039ddf6417255d803d01b3084348d51
John <johnopana2016@gmail.com>
2019-11-11 19:30:35 +0300

    add total price

--- js/script.js
+++ js/script.js
@@ -25,5 +25,28 @@ $(document).ready(function() {
           "</td>" +
           "</tr>"
       );
+      $(".toPay1").text(totalPrice);
+      $(".toPay").text(totalPrice+200);
   });
+      $("button#complete-order").click(function(){
+          $(".delivery").show();
+          $("button#complete-order").attr("disabled", true);
+      });
+      $("button#yes").click(function(){
+          $(".delivery").hide();
+          $(".location").show();
+      });
+      $("button#location-submit").click(function(){
+          $(".location").hide();
+          var location = $("input#yourLocation").val();
+          $("p#text").text("Your pizza will be delivered to "+location + ".");
+          $(".payment").show();
+      });
+      $("input#yourLocation").val("");
+      $("button#no").click(function(){
+          $(".delivery").hide();
+          $(".noDelivery").show();
+      });
 });
+
+  
