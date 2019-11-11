09131ab195c77d4f72838ca1e847ec46c56ad7e3
John <johnopana2016@gmail.com>
2019-11-11 11:56:47 +0300

    modify script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 function calc(){
-    
+var capsicum =document.getElementById("img1") .value;
+var periChiken =document.getElementById("img2") .value;
+var barbeque =document.getElementById("img3") .value;
+var sausage =document.getElementById("img4") .value;
+var chickenTikka=document.getElementById("img5") .value;
+var chickenRasher=document.getElementById("img6") .value;
+var total= ("img1"+"img2"+"img3"+"img4"+"img5"+"img6");
+document.getElementById("total").value="total";
 }
